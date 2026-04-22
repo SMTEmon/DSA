2dce2668df8c99770ecc2e0a2c73f3af4adda3da
SMTEmon <156873907+SMTEmon@users.noreply.github.com>
2026-04-22 19:31:52 +0600

    Add: countSort

---
+++ Algorithms/Sorting/countSort.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+std::vector<int> countSort(std::vector<int> vec)
+{
+
+    // min max
+    int min = INT32_MAX, max = INT32_MIN;
+    size_t sizee = vec.size();
+    for (size_t i = 0; i < sizee; i++)
+    {
+        if (min > vec[i])
+            min = vec[i];
+        if (max < vec[i])
+            max = vec[i];
+    }
+
+    int sizeOfCount = max - min + 1;
+
+    std::vector<int> count(sizeOfCount);
+
+    int offset = 0;
+
+    // if (min < 0) // we actually don't need to check this, don't touch if it works (mathematically)
+    offset = -min; // incase we have negative
+
+    // count up
+
+    for (size_t i = 0; i < sizee; i++)
+    {
+        count[(vec[i] + offset)]++;
+    }
+
+    for (int i = 1; i < sizeOfCount; i++)
+    {
+        count[i] += count[i - 1]; // change to prefix sum
+    }
+
+    std::vector<int> result_vec(sizee);
+
+    for (int i = sizee - 1; i >= 0; i--) // Don't use size_t here, size_t i = 0; i--; don't ask me wtf will happen next
+    {
+        result_vec[--count[vec[i] + offset]] = vec[i];
+    }
+
+    return result_vec;
+}
+
+void printVector(std::vector<int> &vec)
+{
+    int sizeOfVector = vec.size();
+    for (int i = 0; i < sizeOfVector; i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+    std::vector<int> v = {6, 3, -3, 5, 9, 2};
+    std::vector<int> res = countSort(v);
+    printVector(res);
+}
